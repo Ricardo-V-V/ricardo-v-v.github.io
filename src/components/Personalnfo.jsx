6317b6a94e65f9d935863bd17f9a6e311e181cc4
Ricardo-V-V <106278646+Ricardo-V-V@users.noreply.github.com>
2024-05-15 15:03:26 -0600

    Finishing up Personalnfo component

--- src/components/Personalnfo.jsx
+++ src/components/Personalnfo.jsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom'
+import SocialLinks from './SocialLinks'
+
 export default function PersonalInfo() {
 	return (
 		<div className='personal-info-wrap'>
@@ -6,6 +9,19 @@ export default function PersonalInfo() {
 			</div>
 			<h4 className='personal-info-name color-black'>Ricardo Villalobos</h4>
 			<span className='personal-info-job color-black'>Front-end Developer</span>
+			<SocialLinks />
+			<ul className='personal-info-menu'>
+				<li>
+					<Link className='personal-info-menu-link' to='/'>
+						<div className='personal-info-menu-icon'>A</div>About
+					</Link>
+				</li>
+				<li>
+					<Link className='personal-info-menu-link' to='/portfolio'>
+						<div className='personal-info-menu-icon'>P</div>Portfolio
+					</Link>
+				</li>
+			</ul>
 		</div>
 	)
 }
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,33 @@
+export default function SocialLinks() {
+	return (
+		<ul className='personal-info-social-links'>
+			<li>
+				<a
+					href='https://github.com/Ricardo-V-V'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					G
+				</a>
+			</li>
+			<li>
+				<a
+					href='mailto:ricvillalobosv@gmail.com'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					M
+				</a>
+			</li>
+			<li>
+				<a
+					href='https://www.linkedin.com/in/ricardo-villalobos'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					I
+				</a>
+			</li>
+		</ul>
+	)
+}
