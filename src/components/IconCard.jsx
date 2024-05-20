b2a90032ac8dac00cb7a26ab68666f6a340e0190
Ricardo-V-V <106278646+Ricardo-V-V@users.noreply.github.com>
2024-05-20 11:30:38 -0600

    Finish About Cards

--- src/components/IconCard.jsx
+++ src/components/IconCard.jsx
@@ -2,9 +2,9 @@ export default function IconCard({ title, description, icon }) {
 	return (
 		<div className='icon-card-wrap'>
 			<div className='personal-info-menu-icon icon-card-icon'>{icon}</div>
-			<div className='icon-card-body'>
-				<h4>{title}</h4>
-				<p>{description}</p>
+			<div>
+				<h4 className='mb-10'>{title}</h4>
+				<p className='mb-0'>{description}</p>
 			</div>
 		</div>
 	)
